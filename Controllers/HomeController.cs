962a8c7d13561887a628dba908207d63a23f81a6
MAGALI WALDFOGIEL <49553387@ort>
2026-04-23 08:09:43 -0300

    cambios

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,12 +15,39 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewBag.Grupo = new grupo(); 
         return View();
     }
-
+    
+    public IActionResult SelectIntegrante(int dni)
+    {
+        return View();
+    }
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult selectIntegrante(int dni)
+    {
+        // Inicializar grupo (carga datos manuales) y obtener el diccionario
+        grupo g = new grupo();
+        var integrantes = g.devolverIntegrantes();
+
+        // Buscar el integrante por DNI
+        Integrante integrante = null;
+        if (integrantes != null && integrantes.ContainsKey(dni))
+        {
+            integrante = integrantes[dni];
+        }
+
+        // Cargar en ViewBag: la información del integrante y el DNI buscado
+        ViewBag.Integrante = integrante;
+        ViewBag.DNI = dni;
+
+        // Retornar la vista infoIntegrante
+        return View("infoIntegrante");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
