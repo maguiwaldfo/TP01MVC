683cf26b8bae14ee4892eb0ea69d555dbf9b5e80
MAGALI WALDFOGIEL <49553387@ort>
2026-04-29 08:21:16 -0300

    cambios

--- Models/grupo.cs
+++ Models/grupo.cs
@@ -18,11 +18,11 @@ namespace TP01.Models
       
      List<string> famIara = new List<string> { "Mamá Carla" };
      List<string> integIara = new List<string> { "Jugar al hockey" };
-     Integrante integrante1 = new Integrante(50289096, "Iara Cohen Lozie", new DateTime(2010, 5, 8), "iara.png", famIara, integIara);
+     Integrante integrante1 = new Integrante(50289096, "Iara Cohen Lozie", new DateTime(2010, 5, 8), "Iara.png", famIara, integIara);
 
       List<string> famMagali = new List<string> { "Mamá Estefania" };
       List<string> integMagali = new List<string> { "Bailar" };
-      Integrante integrante2 = new Integrante(49553387, "Magali Waldfogiel", new DateTime(2009, 7, 24), "magui.png", famMagali, integMagali);
+      Integrante integrante2 = new Integrante(49553387, "Magali Waldfogiel", new DateTime(2009, 7, 24), "Magui.png", famMagali, integMagali);
 
       Integrantes.Add(integrante1.obtenerDni(), integrante1);
       Integrantes.Add(integrante2.obtenerDni(), integrante2);
